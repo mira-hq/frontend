43587fb62056727e4ccfd243f3d4c49b3fc65aab
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-03-06 00:27:03 -0800

    Add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Spinner from "../components/Spinner";
 import { Banner } from "../components/Banner";
 import { Type } from "../components/Type";
 import { initializeApollo } from "../lib/ApolloClient";
+import Head from "next/head";
 
 export default function Home(): React.ReactNode {
   const { loading, error, data } = useFindServersQuery({
@@ -28,13 +29,21 @@ export default function Home(): React.ReactNode {
   ];
 
   return (
-    <div className="bg-white dark:bg-black min-h-screen w-screen flex">
-      <div className={"container mx-auto px-10"}>
-        {error && <Banner message={JSON.stringify(error)} type={Type.DANGER} />}
-        {loading && <Spinner />}
-        <CardList cards={serverCards} />
-        <CardList cards={addServerCard} />
+    <>
+      <Head>
+        <title>Home | Mira HQ</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="bg-white dark:bg-black min-h-screen w-screen flex">
+        <div className={"container mx-auto px-10"}>
+          {error && (
+            <Banner message={JSON.stringify(error)} type={Type.DANGER} />
+          )}
+          {loading && <Spinner />}
+          <CardList cards={serverCards} />
+          <CardList cards={addServerCard} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
